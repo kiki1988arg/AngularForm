5cf2b167b32f91619d3250b82a01a4086f0356fd
kiki1988arg <ezequielarakaki@hotmail.com>
2017-04-12 17:49:41 -0300

    Submit the form with ngSubmit

--- AngularForm/src/app/app.component.ts
+++ AngularForm/src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<hero-form></hero-form>'
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent { }
--- AngularForm/src/app/app.module.ts
+++ AngularForm/src/app/app.module.ts
@@ -1,20 +1,17 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroFormComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
